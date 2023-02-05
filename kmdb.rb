4eb8ef1b267ebbff82cbd880da9cdb5c4b733196
ronapats <ronapat.srisupavanich@kellogg.northwestern.edu>
2023-02-05 08:52:36 +0000

    final

--- kmdb.rb
+++ kmdb.rb
@@ -290,7 +290,7 @@ for movie in movies
     rated = movie["rated"]
     studio = Studio.find_by({"id" => movie["studio_id"]})
     studio_name = studio["name"]
-    puts "#{title}    #{year_released}   #{rated}   #{studio_name}"
+    puts "#{title}  #{year_released} #{rated} #{studio_name}"
 end
 
 # Prints a header for the cast output
@@ -301,3 +301,12 @@ puts ""
 
 # Query the cast data and loop through the results to display the cast output for each movie.
 # TODO!
+roles = Role.all
+for role in roles
+    movie = Movie.find_by({"id" => role["movie_id"]})
+    movie_title = movie["title"]
+    actor = Actor.find_by({"id" => role["actor_id"]})
+    actor_name = actor["name"]
+    character_name = role["character_name"]
+    puts "#{movie_title}    #{actor_name}   #{character_name}   "
+end
